d6015bd615c220ff5439bef52d43561b92165d92
Zen <innovationlife.net@gmail.com>
2016-01-06 11:00:33 +0900

    Add new page.

--- app/controllers/upload_files_controller.rb
+++ app/controllers/upload_files_controller.rb
@@ -3,4 +3,32 @@ class UploadFilesController < ApplicationController
   def index
     @uploadFiles = UploadFile.all
   end
+
+  def show
+    @uploadFile = UploadFile.find(params[:id])
+  end
+
+  def new
+    @uploadFile = UploadFile.new
+  end
+
+  def create
+    @uploadFile = UploadFile.new(upload_file_params)
+
+    if @uploadFile.save
+      redirect_to @uploadFile
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def upload_file_params
+    params.require(:upload_file).
+      permit(:name,
+             :expiry_date,
+             :password,
+             :private_mode)
+  end
 end
